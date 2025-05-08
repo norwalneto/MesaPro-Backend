0a30090164597570e77feec8c889bc39f3659a46
norwalneto <norwalgomess@gmail.com>
2025-05-08 08:20:31 -0300

    configuração inicial do SaaS

---
+++ src/main/java/com/nwlsoftwarehouse/mesapro/MesaproApplication.java
@@ -0,0 +1,13 @@
+package com.nwlsoftwarehouse.mesapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesaproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nwlsoftwarehouse/mesapro/MesaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwlsoftwarehouse.mesapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
