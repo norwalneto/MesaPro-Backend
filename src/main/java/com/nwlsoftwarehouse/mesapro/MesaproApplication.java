7831bf7a6e152d4c798048bf1683ad68477acd49
norwalneto <norwalgomess@gmail.com>
2025-05-08 22:22:38 -0300

    CrCriando conexão com banco de dados usando perfil

--- src/main/java/com/nwlsoftwarehouse/mesapro/MesaproApplication.java
+++ src/main/java/com/nwlsoftwarehouse/mesapro/MesaproApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MesaproApplication {
 
 	public static void main(String[] args) {
+		System.out.println("MYSQL HOST: " + System.getenv("MYSQLHOST"));
 		SpringApplication.run(MesaproApplication.class, args);
 	}
 
